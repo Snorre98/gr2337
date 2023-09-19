c0a3cdfdb98ab66c2a6f84ca49f7c6ea37e1ae91
Jacob Benjamin Alveberg <jacobbal@stud.ntnu.no>
2023-09-19 14:38:35 +0200

    Created loadFile function #21

--- core/src/main/java/core/FileHandler.java
+++ core/src/main/java/core/FileHandler.java
@@ -1,6 +1,9 @@
 package core;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class FileHandler {
 
@@ -15,5 +18,31 @@ public class FileHandler {
             System.err.println("Saving Error");
         }
     }
-    
+
+    public static void loadFile(AlbumReviewList ar){
+        try {
+            File f = new File("albumReviews.txt");
+            Scanner scanner = new Scanner(f);
+            while(scanner.hasNextLine()){
+                String[] s = scanner.nextLine().split("%%% ");
+                String album = s[0];
+                int review = Integer.parseInt((s[1]));
+                ar.addAlbumReview(album, review);
+            
+            }
+            scanner.close();
+        }catch (FileNotFoundException e) {
+      System.out.println("Something has gone wrong in loading");
+      e.printStackTrace();
+        }
+    }
+
+    public static void main(String args[]){
+        AlbumReviewList ar = new AlbumReviewList();
+        System.out.println(ar.albumReviews.size());
+        loadFile(ar);
+        System.out.println(ar.albumReviews.size());
+        
+
+    }
 }
