599ee9a7bb4f45d930ae30da303ae97e200e2d48
sebaskem <sebaskem@stud.ntnu.no>
2023-09-28 15:56:04 +0200

    Implemented AlbumReview- and AlbumReviewListSerializer, and AlbumReviewModule in json folder #53

---
+++ core/src/main/java/json/AlbumReviewModule.java
@@ -0,0 +1,20 @@
+package json;
+
+import com.fasterxml.jackson.core.util.VersionUtil;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+
+import core.AlbumReview;
+import core.AlbumReviewList;
+
+public class AlbumReviewModule extends SimpleModule {
+  private static final String NAME = "CustomIntervalModule";
+  private static final VersionUtil VERSION_UTIL = new VersionUtil() {
+  };
+
+  public AlbumReviewModule() {
+    super(NAME, VERSION_UTIL.version());
+    addSerializer(AlbumReview.class, new AlbumReviewSerializer());
+    addSerializer(AlbumReviewList.class, new AlbumReviewListSerializer());
+  }
+  
+}
