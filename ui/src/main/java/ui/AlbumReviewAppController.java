349091542a17dc0ebb349ab8edfb0695506768ec
sebaskem <sebaskem@stud.ntnu.no>
2023-10-04 13:34:41 +0200

    Resolved merge conflict ##53

--- ui/src/main/java/ui/AlbumReviewAppController.java
+++ ui/src/main/java/ui/AlbumReviewAppController.java
@@ -22,6 +22,10 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import json.LoadFromFile;
+import json.WriteToFile;
+
 
 public class AlbumReviewAppController implements Initializable {
 
