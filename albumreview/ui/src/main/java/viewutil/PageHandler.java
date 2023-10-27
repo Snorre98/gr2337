76e4e22bf2c56b9cbbfb26e37e104668265a70fd
Jacob Benjamin Alveberg <jacobbal@stud.ntnu.no>
2023-10-27 15:22:09 +0200

    updated pagehandler to have 2 load functions #80

--- albumreview/ui/src/main/java/viewutil/PageHandler.java
+++ albumreview/ui/src/main/java/viewutil/PageHandler.java
@@ -1,20 +1,45 @@
 package viewutil;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class PageHandler{
-  private String username;
-  private String password;
 
-  public void setUser(String username, String password) {
-    this.username = username;
-    this.password = password;
-  }
+  AlbumListController albumListController;
+  AlbumController albumController;
+  
+public void loadAlbumList(String username){
+  albumListController.setUser(username);
+    try {
+      FXMLLoader loader;
+      loader = new FXMLLoader(getClass().getResource("/fxml/AlbumList.fxml"));
+      Parent root = (Parent) loader.load();
+      Stage stage = new Stage();
+      stage.setTitle("The Albums");
+      stage.setScene(new Scene(root));
+      stage.show();
+    } catch (Exception e) {
+      System.out.println("HER GIKK NOE FEIL, men knappen kjører funksjonen");
+    }
+}
 
-  public String getUsername() {
-    return username;
-  }
+public void loadAlbum(String username){
+  albumController.setUser(username);
+    try {
+      FXMLLoader loader;
+      loader = new FXMLLoader(getClass().getResource("/fxml/Album.fxml"));
+      Parent root = (Parent) loader.load();
+      Stage stage = new Stage();
+      stage.setTitle("The Album");
+      stage.setScene(new Scene(root));
+      stage.show();
+    } catch (Exception e) {
+      System.out.println("HER GIKK NOE FEIL, men knappen kjører funksjonen");
+    }
+}
 
-  public String getPassword() {
-    return password;
-  }
+
+
 }
